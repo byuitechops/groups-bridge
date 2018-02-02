ff32342d4f78c24ce999e95fdd03ff104e5f93f5
Benjamin Earl <ear15002@byui.edu>
2018-02-02 13:45:06 -0700

    Actually tested and made it work

--- main.js
+++ main.js
@@ -5,88 +5,131 @@
 */
 
 /* Put dependencies here */
-const async = require('async')
-const fs = require('fs')
-var request = require('request')
-const { URL } = require('url')
-var cookies = [
-    'd2lSessionVal=QbAJFTrPfZKs5zo1PbRRuOp8M;',
-    'd2lSecureSessionVal=M76AlKC0PV6Xz3NDzIuBeiChM;',
-]
+const async = require('async');
+const { URL } = require('url');
+var request = require('request');
+var course;
 
 /* Include this line only if you are going to use Canvas API */
-//const canvas = require('canvas-wrapper');
+const canvas = require('canvas-wrapper');
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
-function addCookies(cookies) {
+function addCookies(subdomain,cookies) {
     var j = request.jar();
-    cookies.forEach(c => j.setCookie(request.cookie(c), 'https://byui.brightspace.com/'))
+    cookies.forEach(c => j.setCookie(request.cookie(c), `https://${subdomain}.brightspace.com/`));
     request = request.defaults({
         jar: j
-    })
+    });
 }
 
-function D2LGET(url) {
-    var url = new URL(url, 'https://byui.brightspace.com/').href
-    return new Promise((resolve,reject) => {
-        request.get(url, function (err, res, body) {
-            if (err) return reject(err)
-            try {
-                // For some reason the json parse dosen't work
-                var data = JSON.parse(body)
-                resolve(data)
-            } catch (e) {
-                try {
-                    // So when that fails i'm cheating and using eval
-                    var data = eval(body)
-                    resolve(data)
-
-                } catch(e){
-                    reject(e)
-                }
+function D2LGET(subdomain,url,cb) {
+    url = new URL(url,`https://${subdomain}.brightspace.com/`).href;
+    course.message(`Making a request to ${url}`);
+    request.get(url, function (err, res, body) {
+        console.log(res.statusCode,body);
+        if (err) return cb(err);
+        if (res.statusCode != 200){
+            return cb(res.statusCode+' '+body)
+        }
+        try {
+            if(body.length){
+                var data = JSON.parse(body);
+                cb(null,data);
+            } else {
+                cb(null,body);
             }
-        })
-    })
+        } catch (e) {
+            cb(body+' '+ e);
+        }
+    });
 }
 
-async function getCatagories(ou, version) {
+function getCatagories(subdomain, ou, version,cb) {
     // Shortening our urls a little bit
-    var tag = `/d2l/api/lp/${version || "1.20"}/${ou}`
+    var tag = `/d2l/api/lp/${version || '1.20'}/${ou}`;
 
     // Getting every group catagory
-    var catagories = await D2LGET(`${tag}/groupcategories/`)
-    for(var i = 0; i < catagories.length; i++){
-        var url = `${tag}/groupcategories/${catagories[i].GroupCategoryId}/groups/`
-        catagories[i].Groups = await D2LGET(url)
-    }
-    return catagories
+    D2LGET(subdomain,`${tag}/groupcategories/`,function(err,data){
+        if(err){
+            course.error(new Error('Couldn\'t get the groupcatagories in d2l: '+err));
+            return cb('Couldn\'t get the groupcatagories in d2l: '+err);
+        }
+
+        var canvasCategories;
+
+        if(data.length){
+            course.message(`Got ${data.length} groupcatagories`);
+            // Mapping them to the canvas settings
+            canvasCategories = data.map(function(cat){
+                // See the 'notes' file for my notes on this
+                var self_signup = undefined;
+                if(cat.AutoEnroll){
+                    if(cat.RestrictedByOrgUnitId){
+                        self_signup = 'restricted';
+                    } else {
+                        self_signup = 'enabled';
+                    }
+                }
+                return {
+                    name: cat.Name,
+                    group_limit: cat.MaxUsersPerGroup,
+                    self_signup: self_signup,
+                };
+            });
+        } else {
+            course.message('There aren\'t any groupcatagories');
+            canvasCategories = [];
+        }
+		
+        cb(null,canvasCategories);
+    });
 }
 
-
-async function main(){
-    addCookies(cookies)
-    var groups = await getCatagories(237861,"1.20")
-    fs.writeFileSync('groups.json',JSON.stringify(groups))
+function createCategories(courseId,cats, cb){
+    async.map(cats,function(cat,catCB){
+        canvas.post(`/api/v1/courses/${courseId}/group_categories`,cat,function(err,data){
+            // errors are handled later
+            catCB(null,{
+                error:err,
+                data:data,
+                name:cat.name,
+            });
+        });
+    },cb);
 }
 
-main()
+module.exports = (_course, stepCallback) => {
+    /* Used to log successful actions */
+    course = _course;
+    // course.message('moduleName successfully ...');
 
+    /* How to report an error (Replace "moduleName") */
+	
+    var subdomain = 'byui';
+    var cookies = [
+        'd2lSessionVal=jRmidd4JGBVdlodUY7KvjPQJs;',
+        'd2lSecureSessionVal=IY2PJWVZcRaxVbR8zsEIfAffs;',
+    ];
+    addCookies(subdomain,cookies);
+    
+    course.info.D2LOU = course.info.D2LOU || 320004;
 
-//module.exports = (course, stepCallback) => {
-//    /* Create the module report so that we can access it later as needed.
-//    This MUST be done at the beginning of each child module. */
-//    course.addModuleReport('moduleName');
-//
-//    /* Used to log successful actions */
-//    course.success('moduleName', 'moduleName successfully ...');
-//
-//    /* How to report an error (Replace "moduleName") */
-//    // course.throwErr('moduleName', e);
-//
-//    /* You should never call the stepCallback with an error. We want the
-//    whole program to run when testing so we can catch all existing errors */
-//
-//    stepCallback(null, course);
-//};
+    getCatagories(subdomain,course.info.D2LOU,'1.20',function(err,data){
+        if(err) return console.error(err);
+		
+        createCategories(course.info.canvasOU,data,function(err,data){
+            data.forEach(res => {
+                if(res.err){
+                    course.warning(new Error(`couldn't create the ${res.name} group catagory`));
+                } else {
+                    course.log('Group Catagories Created',{
+                        'Group Name': res.name
+                    });
+                }
+            });
+            stepCallback(null, course);
+        });
+    });
+};
